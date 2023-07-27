57afcefbbdb73c76d270e008c1f63d1220fab7fc
Roman0064 <alee93474@gmail.com>
2023-07-27 23:03:18 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,75 +1,24 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
+import { Component } from "react";
+import Searchbar from "./Searchbar/Searchbar";
+import ImageGallery from './ImageGallery/ImageGallery'
 
-import Searchbar from './Searchbar/Searchbar';
-import ImageGallery from './ImageGallery/ImageGallery';
-import Button from './Button/Button';
-import Loader from './Loader/Loader';
-import Modal from './Modal/Modal';
-
-const API_KEY = '37349806-fbd17cc6692905f34ac659bee';
-
-const App = () => {
-  const [images, setImages] = useState([]);
-  const [searchQuery, setSearchQuery] = useState('');
-  const [page, setPage] = useState(1);
-  const [isLoading, setIsLoading] = useState(false);
-  const [showModal, setShowModal] = useState(false);
-  const [modalImageUrl, setModalImageUrl] = useState('');
-
-  useEffect(() => {
-    if (searchQuery !== '') {
-      fetchImages();
-    }
-  }, [searchQuery]);
-
-  const fetchImages = () => {
-    setIsLoading(true);
-
-    axios
-      .get(`https://pixabay.com/api/?q=${searchQuery}&page=${page}&key=${API_KEY}&image_type=photo&orientation=horizontal&per_page=12`)
-      .then((response) => {
-        setImages((prevImages) => [...prevImages, ...response.data.hits]);
-        setPage((prevPage) => prevPage + 1);
-      })
-      .catch((error) => console.error('Error fetching images:', error))
-      .finally(() => setIsLoading(false));
+class App extends Component {
+  state = {
+    images: '',
   };
 
-  const handleSearchSubmit = (query) => {
-    setSearchQuery(query);
-    setPage(1);
-    setImages([]);
+  handleFormSubmit = images => {
+    this.setState({ images });
   };
 
-  const handleLoadMoreClick = () => {
-    fetchImages();
+  render() {
+    return(
+      <div>
+        <Searchbar onSubmit={this.handleFormSubmit}/>
+        <ImageGallery images={this.state.images}/>
+      </div>
+    )
   };
-
-  const handleImageItemClick = (imageUrl) => {
-    setModalImageUrl(imageUrl);
-    setShowModal(true);
-  };
-
-  const handleCloseModal = () => {
-    setShowModal(false);
-  };
-
-  return (
-    <div>
-      <Searchbar onSubmit={handleSearchSubmit} />
-      {images.length > 0 && (
-        <ImageGallery images={images} onItemClick={handleImageItemClick} />
-      )}
-      {isLoading && <Loader />}
-      {images.length > 0 && !isLoading && (
-        <Button onClick={handleLoadMoreClick} />
-      )}
-      {showModal && (
-        <Modal imageUrl={modalImageUrl} onClose={handleCloseModal} />
-      )}
-    </div>
-  );
 };
 
 export default App;
