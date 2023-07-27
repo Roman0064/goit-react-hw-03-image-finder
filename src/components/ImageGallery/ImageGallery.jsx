086e7a693dc2b1d21a2769a5751eb00524b93535
Roman0064 <122211211+Roman0064@users.noreply.github.com>
2023-07-27 14:55:25 +0300

    Update ImageGallery.jsx

---
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import ImageGalleryItem from '../ImageGalleryItem/ImageGalleryItem';
+
+const ImageGallery = ({ images, onItemClick }) => {
+  return (
+    <ul className="gallery">
+      {images.map((image) => (
+        <ImageGalleryItem key={image.id} image={image} onItemClick={onItemClick} />
+      ))}
+    </ul>
+  );
+};
+
+export default ImageGallery;
