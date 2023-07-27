dffd938813fc5c006652c98f221366513b20dcb0
Roman0064 <122211211+Roman0064@users.noreply.github.com>
2023-07-27 14:55:29 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,75 @@
-export const App = () => {
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+import Searchbar from './Searchbar/Searchbar';
+import ImageGallery from './ImageGallery/ImageGallery';
+import Button from './Button/Button';
+import Loader from './Loader/Loader';
+import Modal from './Modal/Modal';
+
+const API_KEY = '37349806-fbd17cc6692905f34ac659bee';
+
+const App = () => {
+  const [images, setImages] = useState([]);
+  const [searchQuery, setSearchQuery] = useState('');
+  const [page, setPage] = useState(1);
+  const [isLoading, setIsLoading] = useState(false);
+  const [showModal, setShowModal] = useState(false);
+  const [modalImageUrl, setModalImageUrl] = useState('');
+
+  useEffect(() => {
+    if (searchQuery !== '') {
+      fetchImages();
+    }
+  }, [searchQuery]);
+
+  const fetchImages = () => {
+    setIsLoading(true);
+
+    axios
+      .get(`https://pixabay.com/api/?q=${searchQuery}&page=${page}&key=${API_KEY}&image_type=photo&orientation=horizontal&per_page=12`)
+      .then((response) => {
+        setImages((prevImages) => [...prevImages, ...response.data.hits]);
+        setPage((prevPage) => prevPage + 1);
+      })
+      .catch((error) => console.error('Error fetching images:', error))
+      .finally(() => setIsLoading(false));
+  };
+
+  const handleSearchSubmit = (query) => {
+    setSearchQuery(query);
+    setPage(1);
+    setImages([]);
+  };
+
+  const handleLoadMoreClick = () => {
+    fetchImages();
+  };
+
+  const handleImageItemClick = (imageUrl) => {
+    setModalImageUrl(imageUrl);
+    setShowModal(true);
+  };
+
+  const handleCloseModal = () => {
+    setShowModal(false);
+  };
+
   return (
     <div>
-      React homework template
+      <Searchbar onSubmit={handleSearchSubmit} />
+      {images.length > 0 && (
+        <ImageGallery images={images} onItemClick={handleImageItemClick} />
+      )}
+      {isLoading && <Loader />}
+      {images.length > 0 && !isLoading && (
+        <Button onClick={handleLoadMoreClick} />
+      )}
+      {showModal && (
+        <Modal imageUrl={modalImageUrl} onClose={handleCloseModal} />
+      )}
     </div>
   );
 };
+
+export default App;
