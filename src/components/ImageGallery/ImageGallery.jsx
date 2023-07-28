20d5f07b68696c2c8f0d4451d44419c4b0c490dd
Roman0064 <122211211+Roman0064@users.noreply.github.com>
2023-07-28 15:53:54 +0300

    Update ImageGallery.jsx

--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -1,36 +1,60 @@
 import React, { Component } from "react";
 import { getImages } from "services/getImages";
 
-// const Status = {
-//   IDLE: 'idle',
-//   PENDING: 'pending',
-//   RESOLVED: 'resolved',
-//   REJECTED: 'rejected',
-// };
+const Status = {
+  IDLE: 'idle',
+  PENDING: 'pending',
+  RESOLVED: 'resolved',
+  REJECTED: 'rejected',
+};
 
 export default class ImageGallery extends Component{
   state = {
     value: '',
     images: [],
+    error: null,
+    status: Status.IDLE,
 
   };
 
   componentDidUpdate(prevProps, prevState){
     const {textSearch} = this.props;
+
     if(prevProps.textSearch !== this.props.textSearch){
-    getImages(textSearch)
-    .then(res => res.json())
-    .then((image) => this.setState({images: image.hits}));
+      this.setState({ status: Status.PENDING });
+
+      getImages(textSearch)
+      .then(res => res.json())
+      .then((image) => this.setState({ images: image.hits, status : Status.RESOLVED }))
+      .catch(error => this.setState({ error, status: Status.REJECTED }));
     };
   };
 
   render() {
-    return( 
-      <div>
-        {this.state.images.map((el)=>(
-          <li><img src={el.webformatURL} alt={el.type} /></li>
-        ))}
-      </div>
-    )
+    const {status, error, images} = this.state;
+
+    if(status === 'idle') {
+      return <div></div>
+    };
+
+    if(status === 'pending') {
+      return <div><h1>Loading...</h1></div>
+    };
+
+    if(status === 'rejected') {
+      return alert('Ooooops', error);
+    }
+
+    if(status === 'resolved') {
+      return( 
+        <div>
+          <ul>
+            {images.map((el)=>(
+              <li><img src={el.webformatURL} alt={el.type} /></li>
+            ))}
+          </ul>
+        </div>
+      )
+    };
   };
 };
