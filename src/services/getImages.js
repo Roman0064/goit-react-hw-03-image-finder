8ab647fe25d3c0c9e54097a479d17a4e8180bdb5
Roman0064 <122211211+Roman0064@users.noreply.github.com>
2023-07-28 16:11:41 +0300

    Update getImages.js

--- src/services/getImages.js
+++ src/services/getImages.js
@@ -3,4 +3,14 @@ const API_KEY = '37349806-fbd17cc6692905f34ac659bee';
 
 export const getImages = (textSearch, page = 1) => {
     return fetch(`${BASE_URL}?key=${API_KEY}&q=${textSearch}&page=${page}&image_type=photo&orientation=horizontal&per_page=12`)
+    .then(response => {
+        if (response.ok) {
+          return response.json();
+        }
+        return Promise.reject(
+            new Error(
+              `Oops... there are no ${textSearch} images matching your search... `
+            )
+        );
+    });
 };
