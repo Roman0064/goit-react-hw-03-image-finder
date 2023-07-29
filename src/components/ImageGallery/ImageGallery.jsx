c3049c312a78820a39de850feaa0c6bed2049a85
Roman0064 <alee93474@gmail.com>
2023-07-29 03:52:43 +0300

    Update ImageGallery.jsx

--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -1,5 +1,11 @@
 import React, { Component } from "react";
+import PropTypes from "prop-types";
 import { getImages } from "services/getImages";
+import {ImageGalleryItem} from '../ImageGalleryItem/ImageGalleryItem'
+import {Button} from '../Button/Button'
+import { Loader } from "components/Loader/Loader";
+// import Modal from "components/Modal/Modal";
+import css from './ImageGallery.module.css'
 
 const Status = {
   IDLE: 'idle',
@@ -9,51 +15,71 @@ const Status = {
 };
 
 export default class ImageGallery extends Component{
-  state = {
-    value: '',
-    images: [],
-    error: null,
-    status: Status.IDLE,
 
+  static propTypes = {
+    textSearch: PropTypes.string.isRequired,
+    handleImageClick: PropTypes.func.isRequired,
   };
+
+
+  state = {
+    images: [],
+    isLoading: false,
+    page: 1,
+    totalPages: 0,
+    status: Status.IDLE,
+  };
+
+  handleLoadMore = () => {
+    this.setState((prevState) => ({page: prevState.page + 1}),this.loadImages);
+  }
 
   componentDidUpdate(prevProps, prevState){
     const {textSearch} = this.props;
 
-    if(prevProps.textSearch !== this.props.textSearch){
-      this.setState({ status: Status.PENDING });
-
-      getImages(textSearch)
-      .then((image) => this.setState({ images: image.hits, status : Status.RESOLVED }))
-      // .catch(error => this.setState({ error, status: Status.REJECTED }));
+    if(prevProps.textSearch !== textSearch){
+      this.setState({page: 1, status: Status.PENDING}, this.loadImages);
     };
   };
 
+  loadImages(){
+    const { textSearch } = this.props;
+    const { page } = this.state;
+
+    getImages(textSearch, page)
+      .then((res) => res.json())
+      .then((data) => {
+        this.setState((prevState) => ({
+          images: [...prevState.images, ...data.hits],
+          totalPages: data.totalHits / 12,
+          status: Status.RESOLVED,
+        }));
+      })
+      .catch(() => {
+        this.setState({status: Status.REJECTED});
+      })
+  };
+
   render() {
-    const {status, error, images} = this.state;
+    const { images, status, page, totalPages } = this.state;
 
-    if(status === 'idle') {
-      return <div></div>
-    };
+      if(status === 'pending') {
+        return <Loader /> ;
+      };
 
-    if(status === 'pending') {
-      return <div><h1>Loading...</h1></div>
-    };
+      if(status === 'rejected') {
+        return <h2>Sorry, something went wrong. Please try again later.</h2>;
+      }
 
-    if(status === 'rejected'){
-      return alert(error, 'OOOOps')
-    }
-
-    if(status === 'resolved') {
-      return( 
-        <div>
-          <ul>
-            {images.map((el)=>(
-              <li><img src={el.webformatURL} alt={el.type} /></li>
-            ))}
-          </ul>
-        </div>
-      )
-    };
+      if(status === 'resolved') {
+        return <div className={css.wrapper}>
+            <ul className={css.ul_item}>
+              {images.map((image) => (
+              <ImageGalleryItem key={image.id} item={image} onClick={() => this.handleImageClick(image)} />
+              ))}
+            </ul>
+            {page < totalPages && <Button onClick={this.handleLoadMore}/>}
+          </div>;
+      };
   };
 };
