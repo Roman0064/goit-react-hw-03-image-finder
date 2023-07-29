23b9d146d7a7d509d92f7dde5fc3b147d1f26d98
Roman0064 <alee93474@gmail.com>
2023-07-29 22:35:16 +0300

    Update ImageGalleryItem.jsx

--- src/components/ImageGalleryItem/ImageGalleryItem.jsx
+++ src/components/ImageGalleryItem/ImageGalleryItem.jsx
@@ -2,12 +2,12 @@ import React from "react";
 import PropTypes from "prop-types";
 import css from './ImageGalleryItem.module.css'
 
-export const ImageGalleryItem = ({ item }) => {
+export const ImageGalleryItem = ({ item, onClick }) => {
     const { webformatURL, tags, } = item;
 
     return (
         <li className={css.li_item}>
-            <img src={webformatURL} alt={tags} className={css.ImageGalleryItem_image}/>
+            <img src={webformatURL} alt={tags} className={css.ImageGalleryItem_image } onClick={() => onClick(item)}/>
         </li>
     );
 };
