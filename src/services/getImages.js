5777ecabb02a91aa09c1df5ea8e26b1d2b8d5f09
Roman0064 <alee93474@gmail.com>
2023-07-29 03:52:30 +0300

    Update getImages.js

--- src/services/getImages.js
+++ src/services/getImages.js
@@ -1,16 +1,6 @@
 const BASE_URL = 'https://pixabay.com/api/';
 const API_KEY = '37349806-fbd17cc6692905f34ac659bee';
 
-export const getImages = (textSearch, page = 1) => {
+export const getImages = (textSearch, page) => {
     return fetch(`${BASE_URL}?key=${API_KEY}&q=${textSearch}&page=${page}&image_type=photo&orientation=horizontal&per_page=12`)
-    .then(response => {
-        if (response.ok) {
-          return response.json();
-        }
-        return Promise.reject(
-            new Error(
-              `Oops... there are no ${textSearch} images matching your search... `
-            )
-        );
-    });
 };
